d7d68388b924aae2e7f1764169d0ead481e7d3ac
syedsaq <syedsaqlain46@gmail.com>
2026-04-05 20:40:27 +0500

    my first commit

---
+++ CodeMazeRunner/Extensions/ServiceExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace CodeMazeRunner.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services) => services.AddCors(options =>
+        {
+           options.AddPolicy("CorsPolicy", builder =>
+           builder.AllowAnyOrigin()
+          .AllowAnyMethod()
+          .AllowAnyHeader());
+        });
+
+
+    }
+}
